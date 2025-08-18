6ec41e98b3327bdb7b58c9d46020d0794108887f
maurokrugerjr <maurinho_k@hotmail.com>
2025-08-18 09:20:14 -0300

    cria rota pra retornar usuário logado

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,22 @@ Route::get('/', function () {
         'version' => app()->version(),
     ]);
 });
+
+Route::group([
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('logout', [AuthController::class, 'logout']);
+    Route::get('refresh', [AuthController::class, 'refresh']);
+    Route::get('me', [AuthController::class, 'me']);
+    Route::post('register', [AuthController::class, 'register']);
+});
+
+Route::group(
+    [
+        'middleware' => 'auth:api'
+    ],
+    function () {
+        Route::patch('user/profile', [UserController::class, 'profile']);
+    }
+);
